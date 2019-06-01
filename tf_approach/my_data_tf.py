39c09c0c6249bce7b0ea105888285b342659f724
BombaGR <226611@student.pwr.edu.pl>
2019-06-01 20:16:19 +0200

    light at the end of the tunnel

--- tf_approach/my_data_tf.py
+++ tf_approach/my_data_tf.py
@@ -3,34 +3,45 @@ from tensorflow import keras
 from read_data.read_xls import read_xls_ziwm
 import numpy as np
 from sklearn.model_selection import train_test_split
+import pandas as pd
 
-
-raw_data = read_xls_ziwm('/home/bomba/PycharmProjects/ZIwM/data/bialaczka.XLS') #All rows are 410
+raw_data = read_xls_ziwm('/home/bomba/PycharmProjects/ZIwM/data/bialaczka.XLS')  # All rows are 410
 lables = []
 rows = []
 test_lables = []
 test_rows = []
 
-# l_raw_data, test_raw_data = tf.spli(tf.random_shuffle(raw_data))
+ttrain, ttest = train_test_split(raw_data, test_size=0.4, random_state=42)
 
-ttrain, ttest = train_test_split(raw_data, test_size=0.2, random_state=42)
+frames = [ttrain, ttest]
 
-for index, row in raw_data.iterrows():
-    if index < 310:
+random_data = pd.concat(frames)
+
+# tttrain, tttest = train_test_split(random_data, test_size=0.2)
+#
+# frames = [tttrain, tttest]
+#
+# random_data_2 = pd.concat(frames)
+
+# print(len(raw_data))
+# print(len(random_data))
+
+for index, row in random_data.iterrows():
+    if len(lables) < 310:
         lables.append(row['Unnamed: 0'])
-        rows.append([i for i in row[2:22]])
+        rows.append([i / 8 for i in row[2:22]])
     else:
         test_lables.append(row['Unnamed: 0'])
-        test_rows.append([i for i in row[2:22]])
+        test_rows.append([i / 8 for i in row[2:22]])
 
-print(len(lables))
-print(len(rows))
-print(len(test_lables))
-print(len(test_rows))
+# print(len(lables))
+# print(len(rows))
+# print(len(test_lables))
+# print(len(test_rows))
 
 
 model = keras.Sequential([
-    keras.layers.Dense(20),
+    keras.layers.Dense(20, activation=tf.nn.relu),
     keras.layers.Dense(128, activation=tf.nn.relu),
     keras.layers.Dense(20, activation=tf.nn.softmax)
 ])
@@ -40,12 +51,14 @@ model.compile(optimizer='adam',
               metrics=['accuracy'])
 
 x = np.array(rows)
-y = np.array(lables)
 
-model.fit(x, y, epochs=5)
+y = np.array([i-1 for i in lables])
+print(y)
+model.fit(x, y, epochs=400)
 
 test_x = np.array(test_rows)
-test_y = np.array(test_lables)
 
+
+test_y = np.array([i-1 for i in test_lables])
+print(test_y)
 test_loss, test_acc = model.evaluate(test_x, test_y)
-print(test_acc)
