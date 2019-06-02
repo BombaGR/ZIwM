2ea2b1c2217d06d7fde56d7ddd1370f3c5cf09c7
BombaGR <226611@student.pwr.edu.pl>
2019-06-02 14:50:56 +0200

    small clean - extract method

--- tf_approach/my_data_tf.py
+++ tf_approach/my_data_tf.py
@@ -5,28 +5,21 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 import pandas as pd
 
+
+def split_data(data, test_size=0.4, random_state=42):
+    train, test = train_test_split(data, test_size=test_size, random_state=random_state)
+    frames = [train, test]
+    return pd.concat(frames)
+
+
 raw_data = read_xls_ziwm('/home/bomba/PycharmProjects/ZIwM/data/bialaczka.XLS')  # All rows are 410
 lables = []
 rows = []
 test_lables = []
 test_rows = []
 
-ttrain, ttest = train_test_split(raw_data, test_size=0.4, random_state=42)
 
-frames = [ttrain, ttest]
-
-random_data = pd.concat(frames)
-
-# tttrain, tttest = train_test_split(random_data, test_size=0.2)
-#
-# frames = [tttrain, tttest]
-#
-# random_data_2 = pd.concat(frames)
-
-# print(len(raw_data))
-# print(len(random_data))
-
-for index, row in random_data.iterrows():
+for index, row in split_data(raw_data).iterrows():
     if len(lables) < 310:
         lables.append(row['Unnamed: 0'])
         rows.append([i / 8 for i in row[2:22]])
@@ -34,15 +27,10 @@ for index, row in random_data.iterrows():
         test_lables.append(row['Unnamed: 0'])
         test_rows.append([i / 8 for i in row[2:22]])
 
-# print(len(lables))
-# print(len(rows))
-# print(len(test_lables))
-# print(len(test_rows))
-
 
 model = keras.Sequential([
     keras.layers.Dense(20, activation=tf.nn.relu),
-    keras.layers.Dense(128, activation=tf.nn.relu),
+    keras.layers.Dense(130, activation=tf.nn.relu),
     keras.layers.Dense(20, activation=tf.nn.softmax)
 ])
 
@@ -52,13 +40,12 @@ model.compile(optimizer='adam',
 
 x = np.array(rows)
 
-y = np.array([i-1 for i in lables])
+y = np.array([i - 1 for i in lables])
 print(y)
 model.fit(x, y, epochs=400)
 
 test_x = np.array(test_rows)
 
-
-test_y = np.array([i-1 for i in test_lables])
+test_y = np.array([i - 1 for i in test_lables])
 print(test_y)
 test_loss, test_acc = model.evaluate(test_x, test_y)
